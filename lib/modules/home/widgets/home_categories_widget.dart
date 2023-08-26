ebfe348c534b0f66cf84c874f923c26d4aa92190
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2023-08-26 16:23:16 -0400

    Init shared folder

--- lib/modules/home/widgets/home_categories_widget.dart
+++ lib/modules/home/widgets/home_categories_widget.dart
@@ -4,84 +4,9 @@ import 'package:aroma_journey/extra/flutter_flow/flutter_flow_model.dart';
 import 'package:aroma_journey/extra/flutter_flow/flutter_flow_theme.dart';
 import 'package:aroma_journey/extra/flutter_flow/form_field_controller.dart';
 import 'package:aroma_journey/modules/home/pages/home_model.dart';
+import 'package:aroma_journey/modules/shared/shared.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-final animationsMap = {
-  'rowOnPageLoadAnimation1': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      MoveEffect(
-        curve: Curves.elasticOut,
-        delay: 0.ms,
-        duration: 1120.ms,
-        begin: const Offset(-46.0, 0.0),
-        end: const Offset(0.0, 0.0),
-      ),
-    ],
-  ),
-  'textOnPageLoadAnimation1': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      MoveEffect(
-        curve: Curves.elasticOut,
-        delay: 0.ms,
-        duration: 1120.ms,
-        begin: const Offset(-46.0, 0.0),
-        end: const Offset(0.0, 0.0),
-      ),
-    ],
-  ),
-  'rowOnPageLoadAnimation2': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      MoveEffect(
-        curve: Curves.elasticOut,
-        delay: 0.ms,
-        duration: 720.ms,
-        begin: const Offset(0.0, -27.0),
-        end: const Offset(0.0, 0.0),
-      ),
-    ],
-  ),
-  'choiceChipsOnPageLoadAnimation': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      FadeEffect(
-        curve: Curves.easeIn,
-        delay: 0.ms,
-        duration: 1040.ms,
-        begin: 0.0,
-        end: 1.0,
-      ),
-    ],
-  ),
-  'rowOnPageLoadAnimation3': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      MoveEffect(
-        curve: Curves.elasticOut,
-        delay: 0.ms,
-        duration: 1080.ms,
-        begin: const Offset(41.0, 0.0),
-        end: const Offset(0.0, 0.0),
-      ),
-    ],
-  ),
-  'textOnPageLoadAnimation2': AnimationInfo(
-    trigger: AnimationTrigger.onPageLoad,
-    effects: [
-      MoveEffect(
-        curve: Curves.elasticOut,
-        delay: 0.ms,
-        duration: 1230.ms,
-        begin: const Offset(-44.99999999999999, 0.0),
-        end: const Offset(0.0, 0.0),
-      ),
-    ],
-  ),
-};
 
 class HomeCategoriesWidget extends StatefulWidget {
   const HomeCategoriesWidget({
@@ -110,7 +35,6 @@ class _HomeCategoriesWidgetState extends State<HomeCategoriesWidget> {
 
   @override
   Widget build(BuildContext context) {
-    int tag = 0;
     return Padding(
       padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
       child: Column(
